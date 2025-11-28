1873ba0c9ac3b59f1963837daac2ca3918cace3f
rahman13301 <abdrahman13301@gmail.com>
2025-11-28 12:41:51 +0530

    Update EC2 module source and configuration values

--- Terraform modules/main.tf
+++ Terraform modules/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 module "ec2_instance" {
-  source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
-  instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+  source = "./Terraform-Modules"
+  ami_value="ami-0fa3fe0fa7920f68e"
+  instance_type_value="t2.micro"
+  subnet_id_value="subnet-05aff75cc18978861"
 }
